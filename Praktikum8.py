9e0b21ae37959c4a3a25ca3e9d93b4e37be09dce
dwitaaeni <witaaeni06@gmail.com>
2021-12-20 00:25:10 +1100

    Menambahkan File

--- Praktikum8.py
+++ Praktikum8.py
@@ -1,38 +1,21 @@
-# labspy07
-# Pertemuan 12
-
-## Praktikum 8 
-## Buat program sederhana dengan mengaplikasikan penggunaan class. Buatlah class untuk menampilkan daftar nilai mahasiswa, dengan ketentuan:
-## 
-
-### Penjelasan
-- Input untuk mengimport, untuk mendapatkan clear system pada os
-```python
 from os import system
-```
-
-- Input untuk membuat class untuk menampung data mahasiswa beserta atributnya
-```python
-class mahasiswa:   
+class mahasiswa:
     nim=""
     nama=""
     tugas=""
     uts=""
     uas=""
     akhir=""
- ```
-- Input untuk variabel untuk menampung list dari dari object mahasiswa
-```python
+
 pilih=0
 datasiswa=[]
-```
-- Input untuk menampilkan daftar menu mahasiswa
-```python
+
 def menu():
     system("cls")
     print("PROGRAM INPUT NILAI MAHASISWA")
-    print("Menu Utama");
-    print("-"*43)
+    print("-----------------------------------------")
+    print("MENU UTAMA");
+    print("-"*50)
     print("1. Tambah Data")
     print("2. Lihat Data")
     print("3. Ubah Data")
@@ -51,19 +34,19 @@ def menu():
         tampil()
         id_edit = int(input("Input NIM yang akan diupdate "))
         for a in range(0, len(datasiswa)):
-            if id_edit==datasiswa[a].nim:
+            if id_edit==datamahasiswa[a].nim:
                 index_update=a
                 break
         if (index_update > -1):
             print("INPUT DATA YANG DI UPDATE")
             siswa=mahasiswa()
-            siswa.nim=(int(input("Msdukan NIM                      : ")))
+            siswa.nim=(int(input("Masukan NIM                      : ")))
             siswa.nama=(input("Masukan Nama Mahasiswa          : "))
             siswa.tugas=(float(input("Masukan Nilai Tugas              : ")))
             siswa.uts=(float(input("Masukan Nilai Uts              : ")))
             siswa.uas=(float(input("Masukan Nilai Uas              : ")))
             siswa.akhir=siswa.tugas*0.30+siswa.uts*0.35+siswa.uas*0.35
-            datasiswa=[index_update]=siswa
+            datamahasiswa=[index_update]=siswa
             print("Berhasil Update Data Mahasiswa")
         else : print("NIM Tidak Ditemukan")
         input("Kembali Menu Utama")
@@ -80,16 +63,26 @@ def menu():
         if(index_delete > -1):
             del datasiswa[index_delete]
             print("Data Telah Dihapus")
-        else: 
+        else:
             print("NIM Tidak Ditemukan")
             input("Kembali Menu Utama")
             menu()
             menu()
     elif pilih ==5 :
         exit()
-```
-- Input untuk menambahkan data mahasiswa
-```python
+
+def tampil():
+    system("cls")
+    print("DATA MAHASISWA")
+    for data in datasiswa:
+        print("Nim          : "+str(data.nim))
+        print("Nama         : "+data.nama)
+        print("Nilai Tugas  : "+str(data.tugas))
+        print("Nilai UTS    : "+str(data.uts))
+        print("Nilai UAS    : "+str(data.uas))
+        print("Nilai Akhir  : "+str(data.akhir))
+        print("-"*25)
+
 def pilih1():
     ulang = "Y"
     while ulang in("y","Y"):
@@ -105,46 +98,3 @@ def pilih1():
         datasiswa.append(siswabaru)
         ulang=input("Apakah Anda Ingin Mengulang (Y/T)? ")
 menu()
-```
-- Input untuk menampilkan data yang sudah tersimpan ,cara mengaksesnya dengan memilih angka yang sudah disedikan di daftar menu
-```python
-def tampil():
-    system("cls")
-    print("DATA MAHASISWA")
-    for data in datasiswa:
-        print("Nim          : "+str(data.nim)) 
-        print("Nama         : "+data.nama)
-        print("Nilai Tugas  : "+str(data.tugas))
-        print("Nilai UTS    : "+str(data.uts))
-        print("Nilai UAS    : "+str(data.uas))
-        print("Nilai Akhir  : "+str(data.akhir))
-        print("-"*18)
-```
-- Input untuk menghapus data mahasiswa yang sudah dimasukan kedalam menu
-```python
-elif pilih ==4:
-        system("cls")
-        tampil()
-        index_update=-1
-        id_hapus = int(input("Input NIM Yang Akan Dihapus"))
-        for a in range (0, len(data)):
-            if id_hapus==datasiswa[a].nim:
-                index_delete = a
-                break
-        if(index_delete > -1):
-            del datasiswa[index_delete]
-            print("Data Telah Dihapus")
-        else: 
-            print("NIM Tidak Ditemukan")
-            input("Kembali Menu Utama")
-            menu()
-            menu()
-```
-- Input untuk keluar dari program
-```python
-  elif pilih ==5 :
-        exit()
-```
-
-### Selesai
-## Terima kasih
